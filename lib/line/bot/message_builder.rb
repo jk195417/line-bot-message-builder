42b8b66e296409dc62dded824bea16e35cc41056
楊竑昕 <jk195417@gmail.com>
2018-04-05 11:18:05 +0800

    init

---
+++ lib/line/bot/message_builder.rb
@@ -0,0 +1,9 @@
+require "line/bot/message_builder/version"
+
+module Line
+  module Bot
+    module MessageBuilder
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/line/bot/message_builder_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Line::Bot::MessageBuilder do
+  it "has a version number" do
+    expect(Line::Bot::MessageBuilder::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
